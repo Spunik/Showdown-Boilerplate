fa985161725765eb68c192e0ea114add6284a7e2
Wando94 <daniel.wilson11@outlook.com>
2016-04-14 23:41:23 +0100

    Update seticon.js

--- chat-plugins/seticon.js
+++ chat-plugins/seticon.js
@@ -1,4 +1,3 @@
-'use strict';
 var fs = require('fs');
 var selectors;
  
@@ -7,7 +6,7 @@ function writeIconCSS() {
 }
  
 exports.commands = {
-        function icon (target, room, user) {
+        icon: function (target, room, user) {
         if (!this.can('declare')) return this.errorReply("Access denied.");
  
                 var args = target.split(',');
@@ -24,23 +23,4 @@ exports.commands = {
                 writeIconCSS();
         },
         iconhelp: ["/icon [username], [image], [room 1], [room 2], etc. - Sets an icon to a user in chosen rooms. Credits goes to Master Float in this."]
-};
-
-
-		let args = target.split(',');
-		if (args.length < 3) return this.parse('/help seticon');
-		let username = toId(args.shift());
-		let image = 'background: rgba(244, 244, 244, 0.8) url("' + args.shift().trim() + '") right no-repeat;';
-		selectors = '\n\n' + '  #' + toId(args.shift()) + '-userlist-user-' + username;
-		args.forEach(function (room) {
-			selectors += ', #' + toId(room) + '-userlist-user-' + username;
-		});
-		selectors += ' { \n' + '    ' + image +  '\n  }';
-
-		logMoney(user.name + " has set an icon to " + username + ".");
-		this.privateModCommand("(" + user.name + " has set an icon to  " + username + ")");
-		Rooms('staff').add('|raw|' + user.name + " has set an icon to " + username +  ".").update();
-		writeIconCSS();
-	},
-	seticonhelp: ["/seticon [username], [image], [room 1], [room 2], etc. - Sets an icon to a user in chosen rooms."],
 };
