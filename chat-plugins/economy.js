7ce0b2154dae5deec46e56c39c94d00f724713f5
Chirou-Cat <garyoakheart@gmail.com>
2016-04-21 23:38:46 +0100

    unpatched security issue

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -250,7 +250,7 @@ exports.commands = {
 	customsymbol: function (target, room, user) {
 		if (!user.canCustomSymbol && user.id !== user.userid) return this.errorReply("You need to buy this item from the shop.");
 		if (!target || target.length > 1) return this.parse('/help customsymbol');
-		if (target.match(/[A-Za-z\d]+/g) || '|?!+$%@\u2605=&~#\u03c4\u00a3\u03dd\u03b2\u039e\u03a9\u0398\u03a3\u00a9'.indexOf(target) >= 0) {
+		if (target.match(/[A-Za-z\d]+/g) || '|?!+$%@\u2605=&~#∞\u03c4\u00a3\u03dd\u03b2\u039e\u03a9\u0398\u03a3\u00a9'.indexOf(target) >= 0) {
 			return this.errorReply("Sorry, but you cannot change your symbol to this for safety/stability reasons.");
 		}
 		user.customSymbol = target;
